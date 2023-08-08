bea1d68b1d869ff3cf02b3aa5eaef47c9f5467f6
RobsonFeitosa <robson.gw@hotmail.com>
2023-08-08 19:12:31 -0300

    fix: ref

--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -5,7 +5,7 @@ export interface TextInputProps extends ComponentProps<typeof Input> {
   prefix?: string
 }
 
-export const TextInput = forwardRef<ElementRef<typeof Input>, TextInputProps>(({ prefix, ...props, ref }: TextInputProps) => {
+export const TextInput = forwardRef<ElementRef<typeof Input>, TextInputProps>(({ prefix, ...props}: TextInputProps, ref ) => {
   return (
     <TextInputContainer>
       {!!prefix && <Prefix>{prefix}</Prefix>}
